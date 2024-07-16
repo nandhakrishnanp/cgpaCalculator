cc1d66b7ccc0b4c7f69d75bb940d12ddf0b4c304
nandha <Nandhakrishnandev@gmail.com>
2024-07-16 23:35:53 +0530

    added fav-icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="https://t4.ftcdn.net/jpg/03/02/36/89/360_F_302368996_otkNEsq49lDGQUKJN7EuvYHQcHZ2xIsc.jpg" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
