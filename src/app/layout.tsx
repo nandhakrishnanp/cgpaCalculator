de70a619cddc900fe3a3edf6c1b64ae67d93113f
nandha <Nandhakrishnandev@gmail.com>
2025-01-19 15:15:52 +0530

    changed header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CGPA Calculator",
-  description: "Ggpas calculator for students",
+  description: "Collaborative CGPA Calculator BY Nandhakrishnan",
 };
 
 export default function RootLayout({
